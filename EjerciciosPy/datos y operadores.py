7e9044f0190e5e45e36c9080ac66b1e553f0871e
Ricardo Alfredo Casco <138032193+richardsoftware@users.noreply.github.com>
2023-08-20 10:36:20 -0300

    Add files via upload

---
+++ EjerciciosPy/datos y operadores.py
@@ -0,0 +1,47 @@
+# TIPOS DE DATOS str int float bool
+nombre = "Ricardo"
+print(nombre)
+print(type(nombre))
+
+numero = 5
+print(numero)
+print(type(numero))
+
+numeroreal = 3.7
+print(numeroreal)
+print(type(numeroreal))
+
+valor = True
+print(valor)
+print(type(valor))
+
+
+# OPERADORES ARITMÉTICOS
+x = 2
+y = 6
+
+suma = x+y
+print("Este es el resultado de la suma de x + y =", x + y)
+
+suma = x+y
+print(suma)
+print(type(suma))
+
+# 15) Obtener el cociente de la división de 27 entre 4 
+
+a = 27 / 4
+print("Operador 27/4")
+print(a)
+
+print("Operador 27//4")
+print(27 // 4)
+
+
+# Division Resto
+print("Operador 27%4")
+print(27 % 4)
+
+# Division
+print("6*4+3")
+
+print(6 * 4 + 3)
