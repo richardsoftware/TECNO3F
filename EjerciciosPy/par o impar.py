bb500b7485ee2f91217cfd379a6cb4f49e6fce6c
Ricardo Alfredo Casco <138032193+richardsoftware@users.noreply.github.com>
2023-08-20 15:56:20 -0300

    Add files via upload

---
+++ EjerciciosPy/par o impar.py
@@ -0,0 +1,10 @@
+#par o impar
+numero = int(input("Ingrese un numero natural: "))
+
+print(f"Ud. ingreso el numero {numero}")
+
+
+if numero % 2 == 0:
+    print("El número es par")
+else:
+    print("El número es impar")
